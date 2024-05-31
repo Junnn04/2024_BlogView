96cb9ae6ffefcba18f5611a6ca317df6aac616de
Jun <wd1995vo421@gmail.com>
2024-05-31 16:44:24 +0800

    index

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    //分頁按鈕可點擊（pagination clickable）
+    clickable: true
+  },
+  grid: {
+    // //手機版時，Swiper 排版為 3 列，頁面寬度 >= 768px 時，排版為 1 列（grid rows）
+    rows: 3,
+  },
+  breakpoints: {
+    //頁面寬度 >= 768px 時，（breakpoints）
+    768: {      
+      grid: {
+        rows: 1,
+      },
+      //Swiper 一次可以看到三個 swiper-slide（slidesPerView）
+      slidesPerView: 3,
+      //swiper-slide 左右間距為 24px（spaceBetween）
+      spaceBetween: 24,
+    }
+  }
+});
 
 
   $(document).ready(function(e){
